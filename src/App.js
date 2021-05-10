8673ff90d4ba6ef70ee67abeb898f5f01ad0c1b0
Utsavi009 <utsavikathiriya@gmail.com>
2021-05-10 09:25:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <a target='_blank' href="/instructions.html"> instructions </a>
+      </h1>
+      <br />
+      <img
+        className="logo"
+        src="https://pastebin.com/i/pastebin_logo_side_outline.png"
+        alt="pastebin"
+      />
+      <form className="col-10 offset-1">
+        <div className="form-group">
+          <label htmlFor="api_paste_name">Filename</label>
+          <input
+            type="text"
+            className="form-control"
+            id="api_paste_name"
+            name="api_paste_name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="api_paste_expire_date">Expiration</label>
+          <select
+            className="custom-select"
+            id="api_paste_expire_date"
+            name="api_paste_expire_date"
+          >
+            <option value="10M" selected>
+              10 Minutes
+            </option>
+            <option value="1H">1 Hour</option>
+            <option value="1D">1 Day</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="api_paste_code">Code</label>
+          <textarea
+            class="form-control"
+            id="api_paste_code"
+            name="api_paste_code"
+            rows="3"
+          />
+        </div>
+        <input type="hidden" name="api_paste_private" value="0" />
+        <input type="hidden" name="api_option" value="paste" />
+        <input type="hidden" name="api_user_key" value="" />
+        <input
+          type="hidden"
+          name="api_dev_key"
+          value="dc2d94ed4b463f7ee5e73cb4fac5a18f"
+        />
+        <div className="form-group">
+          <button type="submit" className="btn btn-info">
+            Submit
+          </button>
+        </div>
+        <p>Your link is: ~~~~</p>
+      </form>
     </div>
   );
 }
